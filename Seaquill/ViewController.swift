303340617018f2a0452cdf468fe93d3a6e009cb2
Jeff Kloosterman <jkloosterman@michiganlabs.com>
2016-05-10 19:45:31 -0400

    Base project setup

---
+++ Seaquill/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Seaquill
+//
+//  Created by Jeff Kloosterman on 5/10/16.
+//  Copyright © 2016 Jeff Kloosterman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
